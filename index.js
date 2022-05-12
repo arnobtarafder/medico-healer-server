92944a8e97e9650e4d4efaa2384218574a8c1dfd
arnobtarafder <rmtarnob@gmail.com>
2022-05-12 16:35:36 +0600

    get services data from database

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const cors = require('cors');
+require("dotenv").config()
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middleware
+app.use(cors());
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddzvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+       await client.connect()
+       const servicesCollection = client.db("medico_healer").collection("services");
+
+       app.get("/services", async (req, res) => {
+           const query = {};
+           const cursor = await servicesCollection.find(query).toArray();
+           res.send(cursor)
+       })
+    
+    }   
+    finally{
+
+    }         
+}
+
+run().catch(console.dir)
+
 app.get("/" , (req, res) => {
-    res.send("Hello! I am mr.Developer from medico healer")
+    res.send("Hello! I am Mr.Developer from medico healer")
 })
 
 app.listen(port, () => {
