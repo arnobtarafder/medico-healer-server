0c05da5bb6bdbed30b5cd495db52f863893b7bc6
arnobtarafder <rmtarnob@gmail.com>
2022-05-14 20:52:56 +0600

    add: functionalities of limitation to take appointment

--- index.js
+++ index.js
@@ -17,12 +17,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect()
-        const servicesCollection = client.db("medico_healer").collection("services");
+        const serviceCollection = client.db("medico_healer").collection("services");
+        const bookingCollection = client.db("medico_healer").collection("bookings");
+
+        
+        /**
+         * API Naming Convention
+         * app.get("/booking") // get all booking in this collection. or get more than one or by filter
+         * app.get("/booking/:id") // get a specific booking
+         * app.post("/booking") // add a new booking
+         * app.patch("/booking/:id") //
+         * app.delete("/booking/:id")
+         */
+        
 
         app.get("/services", async (req, res) => {
             const query = {};
-            const collection = await servicesCollection.find(query).toArray();
+            const collection = await serviceCollection.find(query).toArray();
             res.send(collection);
+        })
+
+        app.post("/booking", async (req, res) => {
+            const booking = req.body;
+            const query = {treatment: booking?.treatment, date: booking?.date, patient: booking?.patientName};
+            console.log(booking);
+            const exists = await bookingCollection.findOne(query);
+            if(exists) {
+                return res.send({success: false, booking: exists})
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({success: true, result});
         })
 
     }
