d6ac329d4831ef36db8dea4fa18d216c89dbae62
arnobtarafder <rmtarnob@gmail.com>
2022-05-15 19:40:05 +0600

    secure api to whom want to see by others jwt

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-      return res.status(401).send({ message: 'UnAuthorized access' });
+        return res.status(401).send({ message: 'UnAuthorized access' });
     }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-      if (err) {
-        return res.status(403).send({ message: 'Forbidden access' })
-      }
-      req.decoded = decoded;
-      next();
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
     });
-  }
+}
 
 
 async function run() {
@@ -121,8 +121,12 @@ async function run() {
 
         app.get("/booking", verifyJWT, async (req, res) => {
             const patient = req?.query?.patient;
-            const authorization = req?.headers?.authorization;
-            console.log("auth header", authorization);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                const query = { patient: patient };
+                const bookings = await bookingCollection.find(query).toArray();
+                return res.send(bookings);
+            }
             const query = { patient: patient };
             const bookings = await bookingCollection.find(query).toArray();
 
