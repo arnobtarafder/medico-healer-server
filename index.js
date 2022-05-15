f0db98e546757f60a3f50e383e917882db1808bb
arnobtarafder <rmtarnob@gmail.com>
2022-05-15 19:33:40 +0600

    verify jwt with security functionalities

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddzvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
+
 async function run() {
     try {
         await client.connect()
@@ -62,7 +80,7 @@ async function run() {
                 // steps 5: select slots for the service Bookings: ['', '', '']
                 const bookedSlots = serviceBookings.map(book => book.slot);
                 // service.booked = bookedSlots;
-                // steps 6: select those slots that are not in bookSlots 
+                // steps 6: select those slots that are not in book Slots 
                 const available = service.slots.filter(slot => !bookedSlots.includes(slot));
                 // step 7: set available to slots to make it easier 
                 service.slots = available;
@@ -91,6 +109,8 @@ async function run() {
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
+            const authorization = req?.headers?.authorization;
+            console.log("auth header", authorization);
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
             const exists = await bookingCollection.findOne(query);
             if (exists) {
@@ -101,7 +121,7 @@ async function run() {
         })
 
 
-        app.get("/booking", async (req, res) => {
+        app.get("/booking", verifyJWT, async (req, res) => {
             const patient = req?.query?.patient;
             console.log(patient)
             const query = { patient: patient };
