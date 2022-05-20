aba49ca733403cdd29c401ce2ac36abaeef996d3
arnobtarafder <rmtarnob@gmail.com>
2022-05-20 12:13:52 +0600

    add: delete method for doctor

--- index.js
+++ index.js
@@ -157,6 +157,13 @@ async function run() {
             const result = await doctorCollection.insertOne(doctor);
             res.send(result);
         })
+        
+         app.delete("/doctors/:email", verifyJWT, verifyAdmin, async(req, res) => {
+            const email = req.params.email;
+            const filter = {email: email}
+            const result = await doctorCollection.deleteOne(filter);
+            res.send(result);
+        })
 
 
 
